c21671a3c12478e31008ae9ce4c3b88d248fc0bc
saporito <saporito@OA-S213-10>
2026-03-31 20:12:51 -0400

    Tried the Click to Move challenge from the coding gym.

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour
+{
+    public GameObject player;
+
+    public bool isWorking;
+
+    public Coroutine MovePlayerCoroutine;
+
+    private float distanceToPlayer;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        distanceToPlayer = Vector2.Distance(currentMousePosition, transform.position);
+
+        Debug.Log(distanceToPlayer);
+
+        isWorking = true;
+
+        MovePlayerCoroutine = StartCoroutine(PlayerMovementUpdate());
+    }
+
+    public IEnumerator PlayerMovementUpdate()
+    {
+        while (distanceToPlayer > 0)
+        { 
+            transform.position += (Time.deltaTime + distanceToPlayer) * Vector3.one;        
+        }
+
+        yield return null;
+    }
+}
