25f6e6a718a93ed5860f696777c05c189b4a7659
Leo Saporito <saporito@shernet.sheridancollege.ca>
2026-04-16 22:39:19 -0400

    Made the squeeze animation better

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,41 +3,43 @@ using UnityEngine.InputSystem;
 using System;
 using System.Data;
 using System.Collections;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
+
 {
     public float currentSpeed;
     public float dashSpeed;
-    public float normalSpeed;
+    public float speed;
     public Vector2 directionalInput;
 
     public AnimationCurve squeezeCurve;
     private Coroutine squeezeCoroutine;
-
+    private bool isSqueezing;
     public float attackProgress;
     public float attackDuration;
-
-    public float dashProgress;
-    public float dashDuration;
-
-    private bool isSqueezing;
+    private bool isAttacking;
 
     public Coroutine dashCoroutine;
+    public float dashProgress;
+    public float dashDuration;
+    public float isDash;
+
+    public AnimationCurve rotationCurve;
+    public Vector3 hitRotation;
+    public float hitProgress;
+    public float hitDuration;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentSpeed = normalSpeed;
+        currentSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += (Vector3)directionalInput * Time.deltaTime * currentSpeed;
-
-        if (isSqueezing == true)
-        {
-            squeezeCoroutine = StartCoroutine(SqueezeUpdate());
-        }
+        transform.position += (Vector3)directionalInput * Time.deltaTime * currentSpeed;        
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -47,32 +49,39 @@ public class Player : MonoBehaviour
 
     public void OnAttack(InputAction.CallbackContext context)
     {
-        isSqueezing = true;
-        Debug.Log("Started");
+        if (isAttacking)    return;
+        if (squeezeCoroutine != null)   StopCoroutine(SqueezeUpdate());
+
+        Debug.Log(context);
+
+        squeezeCoroutine = StartCoroutine(SqueezeUpdate());
     }
 
     public IEnumerator SqueezeUpdate()
     {
-        Vector3 currentScale = transform.localScale;
-        if (attackProgress < attackDuration)
+        Vector3 currentScale = transform.localScale;        
+        attackProgress = 0;
+        isAttacking = true;
+
+        while (attackProgress < attackDuration)
         {
             attackProgress += Time.deltaTime;
+            
+            currentScale = squeezeCurve.Evaluate(attackProgress / attackDuration) * Vector3.one;
 
-            while (attackProgress < attackDuration)
-            {
-                transform.localScale = squeezeCurve.Evaluate(attackProgress / attackDuration) * Vector3.one;
+            transform.localScale = Vector3.Lerp(transform.localScale, currentScale, attackDuration);
 
-                yield return null;
-            }
+            yield return null; 
         }
+
+        transform.localScale = currentScale;
+
+        isAttacking = false;
     }
 
     public void OnInteract()
     {
-        if (dashCoroutine != null)
-        {
-            StopCoroutine(dashCoroutine);
-        }
+        if (dashCoroutine != null)  StopCoroutine(dashCoroutine);
 
         dashCoroutine = StartCoroutine(DashUpdate());
     }
@@ -83,6 +92,15 @@ public class Player : MonoBehaviour
 
         yield return new WaitForSeconds(1f);
 
-        currentSpeed = normalSpeed;
+        currentSpeed = speed;
+    }
+
+    public void OnHit()
+    {
+        transform.eulerAngles = hitRotation;
+
+        hitRotation.z += rotationCurve.Evaluate(hitProgress / hitDuration);
+
+        hitRotation = transform.eulerAngles;
     }
 }
